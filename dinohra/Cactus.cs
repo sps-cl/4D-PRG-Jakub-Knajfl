f48bff82065278db8b60937f756fc8d55ebe6c92
knajflspscl <147591340+knajflspscl@users.noreply.github.com>
2023-10-19 21:52:12 +0200

    Add files via upload

---
+++ dinohra/Cactus.cs
@@ -0,0 +1,36 @@
+﻿using dinohra;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dinohra
+{
+    internal class Cactus : GameObject
+    {
+        public Cactus(Vector postion) : base(postion)
+        {
+            Image = new Image(new string[,] {
+                { "# ", "# ", "  ", "# ", "# "},
+                { "# ", "# ", "  ", "# ", "# "},
+                { "# ", "# ", "# ", "# ", "# "},
+                { "# ", "# ", "# ", "# ", "  "},
+                { "  ", "# ", "# ", "  ", "  "},
+                { "  ", "# ", "# ", "  ", "  "}}, Position);
+
+            Collider = new RectCollider(Position, Image.Width, Image.Height);
+            Speed = new Vector(-1.4f, 0);
+        }
+
+        public override void Move()
+        {
+            base.Move();
+            if (Collider.RightEdge < MinBounds.X)
+            {
+                OnDestroy();
+            }
+        }
+    }
+}
+
---
+++ dinohra/IMovable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dinohra
+{
+    interface IMovable
+    {
+        void Move();
+        Vector Position { get; set; }
+        Vector Speed { get; set; }
+    }
+}
